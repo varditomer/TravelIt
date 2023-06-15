4ff15eaef96589e82d24ec7af2f8b8c54064e97c
varditomer <tomer.vardi@gmail.com>
2023-06-15 16:41:45 +0300

    traveling-list cmp remove

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { TravelingFormComponent } from './components/traveling-form/traveling-form.component';
 import { TravelingTableComponent } from './components/traveling-table/traveling-table.component';
-import { TravelingListComponent } from './components/traveling-list/traveling-list.component';
 import { TravelingPreviewComponent } from './components/traveling-preview/traveling-preview.component'; 
 
 @NgModule({
@@ -17,7 +16,6 @@ import { TravelingPreviewComponent } from './components/traveling-preview/travel
     AboutComponent,
     TravelingFormComponent,
     TravelingTableComponent,
-    TravelingListComponent,
     TravelingPreviewComponent
   ],
   imports: [
