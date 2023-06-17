556d733de626012c67794f99f6e4cd8ab599de68
varditomer <tomer.vardi@gmail.com>
2023-06-17 21:17:48 +0300

    add custom valditor to check sDate < eDate

--- src/app/components/traveling-form/traveling-form.component.ts
+++ src/app/components/traveling-form/traveling-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, NgModel, ValidationErrors, Validators } from '@angular/forms';
 import { Country } from 'src/app/models/travel.model';
 import { TravelService } from 'src/app/services/travel.service';
 
@@ -31,11 +31,33 @@ export class TravelingFormComponent {
   }
 
   onAddTravel(addTravelForm: NgForm) {
-    const travelToAdd = {...this.travel}
+    const travelToAdd = { ...this.travel }
     this.travelService.add(travelToAdd)
     addTravelForm.reset()
     this.travel = this.travelService.getEmptyTravel()
   }
+
+  startDateEndDateValidator(startDate: NgModel, endDate: NgModel): void {
+    console.log(`1:`, 1)
+    const validatorFn = (): ValidationErrors | null => {
+
+      const { start_date, end_date } = this.travel
+
+      if (start_date && end_date && start_date > end_date) {
+        return { startDateEndDate: true }; // Validation error key
+      }
+      return null; // No validation error
+    }
+    // We adding the existing validators of the control with our custom validatorFn using Validators setValidators and composing them to a single validate func.
+    if (startDate.control.validator) startDate.control.setValidators(Validators.compose([startDate.control.validator, validatorFn]))
+    else startDate.control.setValidators(validatorFn)
+    startDate.control.updateValueAndValidity()
+
+    if (endDate.control.validator) endDate.control.setValidators(Validators.compose([endDate.control.validator, validatorFn]))
+    else endDate.control.setValidators(validatorFn)
+    endDate.control.updateValueAndValidity()
+  }
+
 
   selectCountry(country: Country) {
     this.isCountryModalOpen = false
